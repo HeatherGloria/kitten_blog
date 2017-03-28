6935256b58b6bc600d8ccad5fd06e8c957ca924f
Lisa Ma <limawebdev@gmail.com>
2017-03-28 15:31:56 -0600

    new user flow is good

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../knex');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,7 +8,31 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next){
-  res.end();
+  var username = req.body.username;
+  knex('users')
+    .where('username', username)
+    .then((data) => {
+      if(data.length === 0){
+        knex('users')
+          .insert({
+            'username': username,
+            'name': username,
+            'email': `${username}@awesome.com`,
+            'profile_pic': 'https://media.giphy.com/media/2xgzOPvrWW7hm/giphy.gif'
+          })
+          .returning('*')
+          .then((newUser) => {
+            res.cookie('userID', newUser[0].id);
+            res.redirect(`/users/${newUser[0].id}`)
+          })
+      }else{
+        res.cookie('userID', data[0].id);
+        res.render('index');
+      }
+      // console.log(data);
+      // res.end();
+    })
+  // res.end();
 })
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var knex = require('../knex')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/:id', function(req, res, next){
+  knex('users')
+    .where('id', req.params.id)
+    .then((data) => {
+      res.render('users', {'username': data[0].username, 'profile_pic': data[0].profile_pic});
+    })
+})
 
 module.exports = router;
