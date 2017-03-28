943fcbda502eb7b1dd3eb1099aaf7e047bd55c46
Lisa Ma <limawebdev@gmail.com>
2017-03-28 15:04:35 -0600

    migrated db

---
+++ migrations/20170328150010_users.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('users', table => {
+    table.increments();
+    table.text('username').notNullable();
+    table.text('name').notNullable().defaultTo('');
+    table.text('email').notNullable().defaultTo('');
+    table.text('profile_pic').notNullable().defaultTo('http://i.imgur.com/BBEvAF2.png');
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
